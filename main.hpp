4a8b18585e1f36db145ba99c71328600cad10ae2
xt2005cc <christian2005cruz@hotmail.com>
2025-04-25 14:37:40 -0700

    1st

--- main.hpp
+++ main.hpp
@@ -1,3 +1,28 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int writeFile()
+{
+    ofstream file("employee.txt");
+    int numEmployees;
+    cout << "Enter number of employees: ";
+    cin >> numEmployees;
+
+    file << numEmployees << endl;
+
+    for (int i = 0; i < numEmployees; i++)
+    {
+        int id;
+        string name, department;
+        double salary;
+
+        cout << "Enter ID, Name, Department, and Salary: "
+        cin >> id >> name> >> department >> salary;
+
+        file << id << " " << name << " " << department << " " << salary << endl;
+    }
+
+    file.close();
+    return numEmployees;
+}
